22aa108d6200a8259e5d2a09337d4a07b42b5135
Christopher Truluck <ctruluck12107@gmail.com>
2021-09-14 12:56:32 +0000

    YEsS

--- script/app.js
+++ script/app.js
@@ -1,7 +1,23 @@
-document.getElementById('about-me').addEventListener('click', () => {
-    location.href = 'path/to/aboutme/page'
-    console.log('test')
+$(function() {
+    $('#start').text('Go!')
+    $('#button').on('click', () => {
+        console.log('TEtstscohdsbvklaj')
+    })
+});
+
+$(function() {
+    5
 })
 
 
 
+//console.print( () =>  {
+//    def function():
+//        class Main() {
+//            public static void main(String[ ], Main, args) {
+//                let var type='int()', "name", size(1000) === (2124131241234);
+//                <!-- defined varible ->
+//                return cout: name
+//            }
+//        }
+//})
