c267abef394d0c02c98b9ada31461f06a66ad4e5
Christopher Truluck <ctruluck12107@gmail.com>
2021-12-10 16:16:10 +0000

    Small changes to the script.

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const colors = ["red", "green", "blue", "orange", "purple"]
+const colors = [
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple"
+]
 
 function changeToRandomColor() {
-    var random = Math.floor(Math.random() * colors.length) 
-    document.body.style.background = colors[random] 
-    document.getElementById("bg-color").innerHTML = `Background color : ${colors[random]}`  
+    var random = Math.floor(Math.random() * colors.length)
+    document.body.style.background = colors[random]
+    document.getElementById("bg-color").innerHTML = `Background color : <span>${colors[random]}</span>`
+    document.getElementById("colorButton").style.backgroundColor = colors[random]
 }
 
 
-window.onload=() => {
+window.onload = () => {
     changeToRandomColor()
     var btn = document.getElementById("colorButton")
     btn.addEventListener("click", changeToRandomColor, true)
